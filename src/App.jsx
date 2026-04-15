26673dd740e75c93a691ea28260780fac93b3026
Lucky Ubaidillah <luckytrusted8@gmail.com>
2026-04-15 14:23:06 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import Detail from "./pages/Detail"; // ✅ TAMBAHAN
+import Detail from "./pages/Detail";
 import Navbar from "./components/Navbar";
 import Checkout from "./pages/Checkout";
 import Success from "./pages/Success";
@@ -15,19 +15,17 @@ function App() {
 
   return (
     <CartProvider>
-      <BrowserRouter>
-        
+      <HashRouter>
         <Navbar search={search} setSearch={setSearch} />
 
         <Routes>
           <Route path="/" element={<Home search={search} />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/product/:id" element={<Detail />} /> {/* ✅ TAMBAHAN */}
+          <Route path="/product/:id" element={<Detail />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/success" element={<Success />} />
         </Routes>
-
-      </BrowserRouter>
+      </HashRouter>
     </CartProvider>
   );
 }
